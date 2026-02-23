ea1b2f7a27cfe2a61025ef9086e8a97fc5db900e
PrateekRajput12 <ficredinn@gmail.com>
2026-02-23 19:57:49 +0530

    state and counter

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - setup 
 - hello world 
 - components and layout and index in app
+
+# day 2 
+- useState 
+- counter 
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,18 @@
-import { StyleSheet, View } from "react-native";
-import ProfileCard from "../components/ProfileCard";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(true)
   return (
     <View style={styles.container}>
-      <ProfileCard
-        name="Prateek Rajput"
-        city="Mathura"
-        skill="MERN Stack Developer"
-      />
-
-      <ProfileCard
-        name="Rahul Sharma"
-        city="Delhi"
-        skill="Frontend Developer"
-      />
+      {show && <Text>Welcome Prateek</Text>}
+      <Text style={styles.title}>Counter App</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button title="Increase" onPress={() => setCount(count + 1)}></Button>
+      <View style={{ height: 10 }}></View>
+      <Button title="Descrease" onPress={() => setCount(count - 1)}></Button>
+      <Button title={show ? "Hide Text" : "Show Text"} onPress={() => setShow(!show)} />
     </View>
   );
 }
@@ -24,6 +22,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  count: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
